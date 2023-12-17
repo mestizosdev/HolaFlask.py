989cad356823679436e67065ee7e5c956f803881
Jorge Luis <jorjoluiso@gmail.com>
2023-12-17 12:55:45 -0500

    [ADD] Elapsed time in seeder

--- chapter_iv/Hola/flaskr/seeder/__init__.py
+++ chapter_iv/Hola/flaskr/seeder/__init__.py
@@ -1,3 +1,4 @@
+import time
 from flaskr.seeder.role_seeder import RoleSeeder
 from flaskr.seeder.user_seeder import UserSeeder
 from flaskr.seeder.module_seeder import ModuleSeeder
@@ -6,9 +7,12 @@ from flaskr.seeder.module_seeder import ModuleSeeder
 class Seeder:
     @staticmethod
     def seed():
+        start = time.time()
         RoleSeeder.create()
         UserSeeder.create()
         ModuleSeeder.create()
+        end = time.time()
+        print(f'{round(end - start, 2)} seconds')
 
     @staticmethod
     def unseed():
