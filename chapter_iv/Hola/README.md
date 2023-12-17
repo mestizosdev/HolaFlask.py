351749984120a13d3fc880f6d4256560bf920e14
Jorge Luis <jorjoluiso@gmail.com>
2023-12-17 09:09:07 -0500

    [ADD] SqlAlchemy declarative model

--- chapter_iv/Hola/README.md
+++ chapter_iv/Hola/README.md
@@ -1,20 +1,29 @@
 # Chapter III
 
+## Content
+- Seeding in database with initial data
+
 ## Resources
 - https://insomnia.rest
-
-## Content
-- Add model and migration
 
 ## Install dependencies
 ```
 pip install -r requirements.txt
 ```
+
 ## Migrations
 ```
 flask --app flaskr db init
 flask --app flaskr db migrate
 flask --app flaskr db upgrade
+```
+### Undo upgrade migrate
+```
+flask --app flaskr db downgrade
+```
+### Remove migration folder for new migration (optional)
+```
+rm -rf migrations
 ```
 
 ## Run
--- chapter_iv/Hola/flaskr/security/model/role.py
+++ chapter_iv/Hola/flaskr/security/model/role.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from flaskr.config.database import db
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
 
-    id = db.Column(db.Integer, db.Identity(start=1), primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    status = db.Column(db.Boolean, nullable=False)
-    create_at = db.Column(
+    id: Mapped[int] = mapped_column(
+        db.Integer, db.Identity(start=1), primary_key=True
+    )
+    name: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
+    status: Mapped[bool] = mapped_column(
+        db.Boolean, nullable=False, default=True
+    )
+    create_at: Mapped[datetime] = mapped_column(
         db.DateTime, nullable=False, server_default=db.func.now()
     )
